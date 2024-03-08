4668f8f8f1731f2a932c74206f299c142433aec6
GitExcited <josedruiz1@gmail.com>
2024-03-08 13:43:48 -0500

    added some of the log in logic from frontend. Also worked on scaffolding for the axios login request

--- MRE_Client/src/main.ts
+++ MRE_Client/src/main.ts
@@ -2,11 +2,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//To store variables in the store
 /*Bootstrpap neews also popperjs */
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App)
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount('#app');
