16b9a36df05300f4ee23731ac7119cb9b664ea8b
Yan Pilon <y.pilon9@gmail.com>
2024-03-08 16:07:09 -0500

    created form routes for backend

--- MRE_Server/routes/forms.js
+++ MRE_Server/routes/forms.js
@@ -4,4 +4,25 @@ const formRouter = express.Router();
 
 
 
+formRouter.post('/reservation',(req,res)=>{
+    const user = req.query.username;
+    
+    //check if vehicle is already reserved or not 
+    //if not reserved create reservation and update vehicle status
+});
+
+formRouter.post('/cancelreservation',(req,res)=>{
+    
+    //find reservation by id
+    //delete reservation and update vehicle 
+});
+
+formRouter.post('/registervehicule',(req,res)=>{
+    //Literally just create db entry
+});
+
+formRouter.post('/',(req,res)=>{
+    
+});
+
 export default formRouter;
