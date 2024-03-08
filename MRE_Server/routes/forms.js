014a0e3665cde4aa2563f7c41cae4736fa46ae81
Yan Pilon <y.pilon9@gmail.com>
2024-03-08 16:59:35 -0500

    finished routes for reservation, cancelreservation and registervechile

--- MRE_Server/routes/forms.js
+++ MRE_Server/routes/forms.js
@@ -1,25 +1,58 @@
 import express from "express";
 import bodyParser from "body-parser";
 import {verifyToken} from "./auth.js"
+import * as dboperations from "../database/operations.js";
 const formRouter = express.Router();
 
 
 
-formRouter.post('/reservation',(req,res)=>{
-    const user = req.query.username;
+formRouter.post('/reservation',async (req,res)=>{
     
+    const {vehicle_id, user_id, rental_start_date, rental_end_date, total_cost, status} = req.query;
     //check if vehicle is already reserved or not 
-    //if not reserved create reservation and update vehicle status
+
+    try{
+    const result = await getVehiclesByFieldValue("vehicle_id", vehicle_id)
+    const vehiclestatus = result[0].status;
+
+    if(vehiclestatus === "available"){
+        await createRental(vehicle_id, user_id, rental_start_date, rental_end_date, total_cost, status)
+        return res.status(201).json({message: "Reservation successfully created."});
+    }else{
+        return res.status(400).json({message: "Error: vehicle already reserved."})
+    }
+    }catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-formRouter.post('/cancelreservation',(req,res)=>{
+formRouter.post('/cancelreservation', async (req,res)=>{
     
     //find reservation by id
     //delete reservation and update vehicle 
+    const rental_id = req.query.rental_id;
+    try{
+        await dboperations.deleteRental(rental_id);
+        return res.status(200).json({message: "Reservation canceled successfully."});
+    }catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+    
 });
 
-formRouter.post('/registervehicule',(req,res)=>{
+formRouter.post('/registervehicle',async (req,res)=>{
     //Literally just create db entry
+    const {make,model,year,license_plate,vehicle_type,color,mileage,status,rental_rate,branch_id,transmission_type} = req.query;
+
+    try{
+        await dboperations.createVehicle(make,model,year,license_plate,vehicle_type,color,mileage,status,rental_rate,branch_id,transmission_type);
+        return res.status(201).json({message: "Vehicle successfully registered."});
+    }catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 formRouter.post('/',(req,res)=>{
