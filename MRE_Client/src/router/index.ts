d80ebf6b264e81591d7050742ad6814ed69bc62e
Alexandre Hachey <alx99102@gmail.com>
2024-03-11 13:34:02 -0400

    maybe fixed?

--- MRE_Client/src/router/index.ts
+++ MRE_Client/src/router/index.ts
@@ -3,13 +3,15 @@ import SignUp from '../views/SignUp.vue';
 import Home from '../views/Home.vue';
 import Services from '../views/Services.vue';
 import RentalForm from '@/views/RentalForm.vue';
-import Confirmation from '@/views/Confirmation.vue'; // Import the Confirmation component
+import Confirmation from '@/views/Confirmation.vue';
+import ToRent from '@/views/ToRent.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/home' }, // Redirect from root to /home
     { path: '/signup', component: SignUp },
+    { path: '/torent', component: ToRent },
     { path: '/home', component: Home },
     { path: '/services', component: Services },
     { path: '/rentalform', component: RentalForm },
